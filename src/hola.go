072ca6a534896b9a0d9c961f8cd131911da0f32d
Alon <alonso.moros@vipera.com>
2026-03-02 10:52:32 +0100

    Init - Declaración de variables

--- src/hola.go
+++ src/hola.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
-
-	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println("Hola mundo")
-	fmt.Println(quote.Hello())
+
+	// Tradicional
+	var nombreAlon, apellidoAlon string = "Alon", "Klk"
+	var edadAlon int = 21
+	fmt.Println("Hello", nombreAlon, apellidoAlon, "with age", edadAlon)
+
+	// Grupal
+	var (
+		nombreAlan   string = "Alan"
+		apellidoAlan string = "Klk"
+		edadAlan     int    = 21
+	)
+	fmt.Println("Hello", nombreAlan, apellidoAlan, "with age", edadAlan)
+
+	// Dinámico
+	var nombreAlen, apellidoAlen, edadAlen = "Alen", "Klk", 21
+	fmt.Println("Hello", nombreAlen, apellidoAlen, "with age", edadAlen)
+
+	// Dinámico v2
+	nombreAlin, apellidoAlin, edadAlin := "Alin", "Klk", 21
+	fmt.Println("Hello", nombreAlin, apellidoAlin, "with age", edadAlin)
 }
